cbe8de6197828e25cdc1b513f111b2671e18c5e7
Star3oy <eduarcaco@hotmail.com>
2023-12-30 12:05:25 -0600

    Creación de las nuevas pantallas para las cartas

---
+++ Spider-Clue/Views/ReasonForFailureCardsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Spider_Clue.Views
+{
+    /// <summary>
+    /// Interaction logic for ReasonForFailureCardsView.xaml
+    /// </summary>
+    public partial class ReasonForFailureCardsView : Window
+    {
+        public ReasonForFailureCardsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
