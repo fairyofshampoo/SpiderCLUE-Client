d1f28e56dace98b30f6b9f224118c961b0570be5
Star3oy <eduarcaco@hotmail.com>
2024-01-06 17:52:39 -0600

    Se ha cambiado la forma de iniciar sesión

--- Spider-Clue/Views/MainMenuView.xaml.cs
+++ Spider-Clue/Views/MainMenuView.xaml.cs
@@ -147,15 +147,12 @@ namespace Spider_Clue.Views
 
         private void ConnectToService()
         {
-            try 
-            {
-                SessionManager.Connect(UserSingleton.Instance.GamerTag);
-            }
-            catch (FaultException<SpiderClueException>)
-            {
-                GoToLoginView();
-                MessageBox.Show("Ya has iniciado sesión", Properties.Resources.ErrorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+                int result = SessionManager.Connect(UserSingleton.Instance.GamerTag);
+                if(result == 0)
+                {
+                    GoToLoginView();
+                    MessageBox.Show("Ya has iniciado sesión", Properties.Resources.ErrorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                } 
         }
 
         private void GoToLoginView()
